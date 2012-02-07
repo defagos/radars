9b5c0fc43c99e870d37bce676ac774e15f22130d
Samuel Défago <defagos@gmail.com>
2012-02-07 21:24:08 +0100

    Added project to illustrate the bug

---
+++ xcdatamodel-xcode4.1-bug/Sources/AppDelegate/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  xcdatamodel-xcode4
+//
+//  Created by Samuel Défago on 07.02.12.
+//  Copyright (c) 2012 Samuel Défago. All rights reserved.
+//
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
