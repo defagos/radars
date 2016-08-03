04d41aa4f347151dbec330a0fe6f2fbc4c368b4e
Samuel Défago <defagos@gmail.com>
2016-08-03 09:56:23 +0200

    Log constraints. This works in a storyboard

--- uninstalled-constraint-bug/uninstalled-constraint-bug/ViewController.m
+++ uninstalled-constraint-bug/uninstalled-constraint-bug/ViewController.m
@@ -3,4 +3,20 @@
 
 @implementation ViewController
 
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection
+{
+    [super traitCollectionDidChange:previousTraitCollection];
+    
+    // Display constraints. An identifier beginning with "debug_" has been set in the storyboard for those constraints
+    // which might not be installed depending on the size class. To keep log entries minimal, those are the only ones we log below
+    NSLog(@"Constraints");
+    for (NSLayoutConstraint *layoutConstraint in self.view.constraints) {
+        if (! [layoutConstraint.identifier hasPrefix:@"debug_"]) {
+            continue;
+        }
+        
+        NSLog(@"  > identifier: %@, active: %@", layoutConstraint.identifier, layoutConstraint.active ? @"YES" : @"NO");
+    }
+}
+
 @end
