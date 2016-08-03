1094a1999b79a3920bf5ffd41e8726ea788c4926
Samuel Défago <defagos@gmail.com>
2016-08-03 09:12:29 +0200

    Create project with basic layout

---
+++ uninstalled-constraint-bug/uninstalled-constraint-bug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  uninstalled-constraint-bug
+//
+//  Created by Samuel Défago on 03/08/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
