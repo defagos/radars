a9115382bd3c24fdc6869697b9b4d51ff9f09979
Samuel Défago <defagos@gmail.com>
2019-08-09 14:22:27 +0200

    Add fourth scenario

--- airplay-button-ios13b6/AirPlayButton/ViewController.swift
+++ airplay-button-ios13b6/AirPlayButton/ViewController.swift
@@ -7,11 +7,37 @@ class ViewController: UIViewController {
     var detector: AVRouteDetector?
     
     // Setup 1:
-    //   - Route picker view
+    //   - MPVolumeView only (deprecated API).
     //
-    // AirPlay icon after enabling a route:
-    //   - iOS 11 - 12: Icon inactive.
-    //   - iOS 13: Icon inactive.
+    // MPVolumeView route icon after enabling a route:
+    //   - iOS 11 - 12: Icon active. ✔︎
+    //   - iOS 13: Icon active. ✔︎
+    //
+    // Comment:
+    // Works well for all iOS versions.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let volumeView = MPVolumeView.init(frame: .zero)
+        volumeView.translatesAutoresizingMaskIntoConstraints = false
+        volumeView.showsRouteButton = true
+        self.view.addSubview(volumeView)
+        
+        NSLayoutConstraint.activate([
+            volumeView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            volumeView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20.0),
+            volumeView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20.0),
+            volumeView.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+    }
+    
+#if false
+    // Setup 2:
+    //   - AVRoutePickerView only.
+    //
+    // AVRoutePickerView route icon after enabling a route:
+    //   - iOS 11 - 12: Icon inactive. ✘
+    //   - iOS 13: Icon inactive. ✘
     //
     // Comment:
     // For all iOS version, AVRoutePickerView state does not seem to be updated correctly if used alone.
@@ -28,15 +54,16 @@ class ViewController: UIViewController {
             routePickerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
         ]);
     }
+#endif
     
 #if false
-    // Setup 2:
-    //   - Route detector (always enabled here for simplicity).
-    //   - Route picker view
+    // Setup 3:
+    //   - AVRouteDetector used (always enabled here for simplicity).
+    //   - AVRoutePickerView.
     //
-    // AirPlay icon after enabling a route:
-    //   - iOS 11 - 12: Icon active.
-    //   - iOS 13: Icon still inactive.
+    // AVRoutePickerView route icon after enabling a route:
+    //   - iOS 11 - 12: Icon active. ✔︎
+    //   - iOS 13: Icon still inactive. ✘
     //
     // Comment:
     // For iOS 11 - 12, AVRoutePickerView state seems to be updated correctly when a route detector is used. For iOS 13
@@ -60,17 +87,16 @@ class ViewController: UIViewController {
 #endif
     
 #if false
-    // Setup 3:
-    //   - Route detector (always enabled here for simplicity).
-    //   - Volume view (deprecated API).
+    // Setup 4:
+    //   - AVRoutePickerView (but NO route detector).
+    //   - MPVolumeView (deprecated API).
     //
-    // AirPlay icon after enabling a route:
-    //   - iOS 11 - 12: Icon active.
-    //   - iOS 13: Icon active
+    // Route icons after enabling a route, both for MPVolumeView and AVRoutePickerView:
+    //   - iOS 11 - 12: Icon active. ✔︎
+    //   - iOS 13: Icon active ✔︎
     //
     // Comment:
-    // For iOS 11 - 12, AVRoutePickerView state seems to be updated correctly when a route detector is used. For iOS 13
-    // this does not work. I feel that MPVolumeView plays the same role as the route detector in Setup 2 above.
+    // For all iOS versions, AVRoutePickerView seems to be magically working when an MPVolumeView is used.
     override func viewDidLoad() {
         super.viewDidLoad()
         
