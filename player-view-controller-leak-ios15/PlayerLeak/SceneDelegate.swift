1bd5529ec0b629fddc6eb20f5caa75b5bb310f09
Samuel Défago <defagos@gmail.com>
2022-03-03 08:06:25 +0100

    Add sample project for AVPlayerViewController leak on iOS 15

---
+++ player-view-controller-leak-ios15/PlayerLeak/SceneDelegate.swift
@@ -0,0 +1,22 @@
+import AVKit
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = AVPlayer(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8")!)
+        playerViewController.player?.play()
+        window?.rootViewController = playerViewController
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            playerViewController.player = AVPlayer(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8")!)
+            playerViewController.player?.play()
+        }
+    }
+}
