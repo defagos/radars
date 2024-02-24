c526bb85872b8bc5282ebdbf0cdc922703fc7d33
Samuel Défago <defagos@gmail.com>
2024-02-24 10:36:08 +0100

    Setup audio session

--- mp3-playback-issue-after-failure/Mp3PlaybackIssue/Mp3PlaybackIssueApp.swift
+++ mp3-playback-issue-after-failure/Mp3PlaybackIssue/Mp3PlaybackIssueApp.swift
@@ -1,7 +1,18 @@
+import AVFoundation
 import SwiftUI
+import UIKit
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+        return true
+    }
+}
 
 @main
 struct Mp3PlaybackIssueApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
